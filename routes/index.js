e72e894719d576fa3d4ae7f70acc1feae85c1f4b
Adron Hall <adronhall@gmail.com>
2016-10-15 16:23:25 -0700

    Added first base sentence description of the Brood Pattern.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index', {title: 'Brood Pattern'});
+  res.render('index');
 });
 
 module.exports = router;

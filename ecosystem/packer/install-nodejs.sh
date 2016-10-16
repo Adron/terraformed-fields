2ed33353bdcd2f740a0e9f8d8a29fc3271da4859
Adron Hall <adronhall@gmail.com>
2016-10-15 17:24:39 -0700

    Changed the nvm install.

--- ecosystem/packer/install-nodejs.sh
+++ ecosystem/packer/install-nodejs.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
 
-# This is for some of the npm install dependencies, which will need to build.
-sudo apt-get install -y build-essential
-
 nvm install 4.6.1
 nvm use 4.6.1
 nvm alias default 4.6.1

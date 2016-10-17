882eeaed3e7f4f162c9950509ec41cfc3722f639
Adron Hall <adronhall@gmail.com>
2016-10-16 20:44:15 -0700

    Adding ecosystem install.

---
+++ ecosystem/systemic-build.sh
@@ -0,0 +1,6 @@
+
+# Terraform installation https://www.terraform.io/intro/getting-started/install.html
+curl -O https://releases.hashicorp.com/terraform/0.7.6/terraform_0.7.6_linux_amd64.zip
+
+# Packer installation https://www.packer.io/docs/installation.html
+curl -O https://releases.hashicorp.com/packer/0.10.2/packer_0.10.2_linux_amd64.zip

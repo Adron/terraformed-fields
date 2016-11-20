580a54040fc1a3efc3a227cb60e74f302cafec6b
Adron Hall <adronhall@gmail.com>
2016-11-19 17:28:26 -0800

    Adding a new sample test.

--- test/the_tests.js
+++ test/the_tests.js
@@ -2,7 +2,13 @@ var assert = require('assert');
 
 describe('Where the important things happen', function () {
   describe('where the functional is good all', function () {
-    it('should be good', function () {
+    it('should be fast!', function () {
+      assert.equal(true, true);
+    });
+    it('should be immutable!', function () {
+      assert.equal(true, true);
+    });
+    it('should be good.', function () {
       assert.equal(true, true);
     });
   });

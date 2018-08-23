971731ec004fd41519ff560ab0fa0c481de97c11
Adron Hall <adronhall@gmail.com>
2018-08-22 18:13:11 -0700

    Built 3 nodes. Next step. Get em' clustered or such.

--- main.tf
+++ main.tf
@@ -38,15 +38,27 @@ resource "google_compute_subnetwork" "prod-sub-west1" {
   network = "${google_compute_network.prod-network.self_link}"
 }
 
+
 // Server Instances
+
+variable "instance_names" {
+  type = "map"
+  default = {
+    "0" = "frank"
+    "1" = "sally"
+    "2" = "muppet"
+  }
+}
+
+
 resource "google_compute_instance" "default" {
-  name = "test"
+  count = "3"
+  name = "${lookup(var.instance_names, count.index)}"
   machine_type = "n1-standard-1"
   zone = "us-west1-a"
 
   tags = [
-    "foo",
-    "bar"]
+    "${lookup(var.instance_names, count.index)}"]
 
   boot_disk {
     initialize_params {
@@ -58,12 +70,13 @@ resource "google_compute_instance" "default" {
   }
 
   network_interface {
-    network = "${google_compute_subnetwork.dev-sub-east1}"
+    subnetwork = "${google_compute_subnetwork.dev-sub-west1.id}"
     access_config {}
   }
 
   metadata {
     node = "cassandra"
+    named = "${lookup(var.instance_names, count.index)}"
   }
 
   metadata_startup_script = "echo hi > /test.txt"

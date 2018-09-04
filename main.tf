42183d2b3f53d62bb34fc6c43dc0b3af3c0e4338
Adron Hall <adron@thrashingcode.com>
2018-09-04 15:27:52 -0700

    Adding kubernetes.

--- main.tf
+++ main.tf
@@ -43,12 +43,11 @@ variable "instance_names" {
   default = {
     "0" = "frank"
     "1" = "sally"
-    "2" = "muppet"
   }
 }
 
 resource "google_compute_instance" "default" {
-  count = "3"
+  count = "2"
   name = "${lookup(var.instance_names, count.index)}"
   machine_type = "n1-standard-1"
   zone = "us-west1-a"
@@ -83,4 +82,48 @@ resource "google_compute_instance" "default" {
       "compute-ro",
       "storage-ro"]
   }
+}
+
+resource "google_container_cluster" "primary" {
+  name               = "marcellus-wallace"
+  zone               = "us-west1-a"
+  initial_node_count = 3
+
+//  additional_zones = [
+//    "us-west1-b",
+//    "us-west1-c",
+//  ]
+
+  master_auth {
+    username = "boss"
+    password = "the*long*enough*password"
+  }
+
+  node_config {
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/compute",
+      "https://www.googleapis.com/auth/devstorage.read_only",
+      "https://www.googleapis.com/auth/logging.write",
+      "https://www.googleapis.com/auth/monitoring",
+    ]
+
+    labels {
+      foo = "bar"
+    }
+
+    tags = ["foo", "bar"]
+  }
+}
+
+# The following outputs allow authentication and connectivity to the GKE Cluster.
+output "client_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.client_certificate}"
+}
+
+output "client_key" {
+  value = "${google_container_cluster.primary.master_auth.0.client_key}"
+}
+
+output "cluster_ca_certificate" {
+  value = "${google_container_cluster.primary.master_auth.0.cluster_ca_certificate}"
 }
--- stateconfig.tf
+++ stateconfig.tf
@@ -6,5 +6,3 @@ terraform {
     credentials = "../infosec/account.json"
   }
 }
-
-
